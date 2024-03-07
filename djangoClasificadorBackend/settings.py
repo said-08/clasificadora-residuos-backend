796da12cafe939804f26d09253eafb5071a45fcd
SAID ELIAS SALCEDO CUPITRA <said.salcedoc@unac.edu.co>
2024-03-06 20:47:47 -0500

    Requirements and pillow installed

--- djangoClasificadorBackend/settings.py
+++ djangoClasificadorBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors auth
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
